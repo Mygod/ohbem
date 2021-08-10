56be3ee315d52a15a4d8800659246c7a599fe232
Mygod <contact-git@mygod.be>
2021-08-09 20:41:37 -0400

    Reformat unit tests

--- test/ohbem.js
+++ test/ohbem.js
@@ -2,42 +2,38 @@ const assert = require('assert');
 const Ohbem = require('../index.js');
 
 describe('Ohbem', () => {
-    const ohbemFut = (async () => {
-        const pokemonData = await Ohbem.fetchPokemonData();
-        return [false, true].map(compactCache => new Ohbem({
-            pokemonData,
-            cachingStrategy: () => [null, compactCache],
-        }));
-    })();
-    it('Elgyem and Audino', async () => {
-        for (const ohbem of await ohbemFut) {
-            let out = ohbem.queryPvPRank(605, 0, 0, 1, 1, 4, 12, 7);
-            assert.strictEqual(out.little.length, 1, `Little cup only has one entry [${ohbem._compactCache}]`);
-            assert.strictEqual(out.little[0].pokemon, 605, `Little cup cannot evolve [${ohbem._compactCache}]`);
-            assert.strictEqual(out.great.length, 3, `GL has 3 entries [${ohbem._compactCache}]`);
+    const pokemonDataFut = Ohbem.fetchPokemonData();
+    for (const [compactCache, ohbemFut] of [false, true].map(compactCache => [compactCache, (async () => new Ohbem({
+        pokemonData: await pokemonDataFut,
+        cachingStrategy: () => [null, compactCache],
+    }))()])) {
+        it(`Elgyem [${compactCache}]`, async () => {
+            const ohbem = await ohbemFut;
+            const out = ohbem.queryPvPRank(605, 0, 0, 1, 1, 4, 12, 7);
+            assert.strictEqual(out.little.length, 1, 'Little cup only has one entry');
+            assert.strictEqual(out.little[0].pokemon, 605, 'Little cup cannot evolve');
+            assert.strictEqual(out.great.length, 3, 'GL has 3 entries');
             assert.strictEqual(Ohbem.filterLevelCaps(out.great, [50, 51]).length, 3,
-                `Useless filtered GL has 3 entries [${ohbem._compactCache}]`);
-            assert.strictEqual(Ohbem.filterLevelCaps(out.great, [50]).length, 2,
-                `L50 GL has 2 entries [${ohbem._compactCache}]`);
-            assert.strictEqual(Ohbem.filterLevelCaps(out.great, [51]).length, 2,
-                `L51 GL has 2 entries [${ohbem._compactCache}]`);
-
-            out = ohbem.queryPvPRank(531, 0, 0, 2, 0, 14, 14, 5);
-            assert.strictEqual(out.little, undefined, `No little cup [${ohbem._compactCache}]`);
-            assert.strictEqual(out.great.length, 2, `GL has 2 entries [${ohbem._compactCache}]`);
-            assert.strictEqual(out.ultra.length, 1, `UL has 1 entires [${ohbem._compactCache}]`);
-            // TODO: finish tests
-        }
-    });
-    it('Functionally Perfect', async () => {
-        for (const ohbem of await ohbemFut) {
+                'Useless filtered GL has 3 entries');
+            assert.strictEqual(Ohbem.filterLevelCaps(out.great, [50]).length, 2, 'L50 GL has 2 entries');
+            assert.strictEqual(Ohbem.filterLevelCaps(out.great, [51]).length, 2, 'L51 GL has 2 entries');
+        });
+        it(`Audino [${compactCache}]`, async () => {
+            const ohbem = await ohbemFut;
+            const out = ohbem.queryPvPRank(531, 0, 0, 2, 0, 14, 14, 5);
+            assert.strictEqual(out.little, undefined, 'No little cup');
+            assert.strictEqual(out.great.length, 2, 'GL has 2 entries');
+            assert.strictEqual(out.ultra.length, 1, 'UL has 1 entires');
+        });
+        // TODO: finish tests
+        it(`Functionally Perfect [${compactCache}]`, async () => {
+            const ohbem = await ohbemFut;
             const out = ohbem.queryPvPRank(661, 0, 0, 1, 15, 15, 14, 1);
-            assert.strictEqual(out.master.length, 3,
-                `Talonflame functionally perfect at level 50/51 [${ohbem._compactCache}]`);
+            assert.strictEqual(out.master.length, 3, 'Talonflame functionally perfect at level 50/51');
             assert.strictEqual(Ohbem.filterLevelCaps(out.master, [50]).length, 1,
-                `Talonflame functionally perfect at level 50 [${ohbem._compactCache}]`);
+                'Talonflame functionally perfect at level 50');
             assert.strictEqual(Ohbem.filterLevelCaps(out.master, [51]).length, 2,
-                `Talonflame functionally perfect at level 51 [${ohbem._compactCache}]`);
-        }
-    });
+                'Talonflame functionally perfect at level 51');
+        });
+    }
 });
